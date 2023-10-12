cfdec6c438b4773c600857019d2ac0271d61527e
Monariih <lucas.monari.lm@outlook.com>
2023-10-11 22:15:34 -0300

    add the game blackjack to the application, waiting for fix the path and link with user.balance

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,10 +8,13 @@
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import router from '../router'
+import store from '@/store'
 
 export function registerPlugins (app) {
+
   loadFonts()
   app
+    .use(store)
     .use(vuetify)
     .use(router)
 }
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import Vuex from 'vuex'
+import getters from './getters'
+import mutations from './mutations'
+import actions from './actions'
+
+const state = {
+  isTitleShowing: true,
+  settings: {
+    deckCount: 6,
+    startingBank: 25,
+    shuffleAfterPercent: 0.75,
+    minimumBet: 1,
+    showBasicStrategy: false
+  },
+  bank: 0,
+  shoe: [],
+  hands: [],
+  activeHandIndex: null,
+  isDealing: false,
+  showDrawer: false
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
+})
