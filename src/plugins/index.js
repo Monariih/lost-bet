69a54e1315ac15403de97bcd134f8bcd682267c7
Monariih <lucas.monari.lm@outlook.com>
2023-11-01 03:14:02 -0300

    remove unused code

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,13 +8,11 @@
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import router from '../router'
-import store from '@/store'
 
 export function registerPlugins (app) {
 
   loadFonts()
   app
-    .use(store)
     .use(vuetify)
     .use(router)
 }
